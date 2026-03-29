6a58925b145a9b95afe54ba6d5bffa7eccd6e972
Abdelsalam <abdonadir500@gmail.com>
2026-03-29 23:38:39 +0200

    Added header component

---
+++ components/header.js
@@ -0,0 +1,43 @@
+import { getUser } from "/services/auth.js";
+
+fetchUser();
+
+async function fetchUser() {
+  try {
+    const user = await getUser();
+    console.log(user);
+    addHeader(user);
+  } catch (error) {
+    console.log("unauthorized");
+  }
+}
+
+function addHeader(user) {
+  const header = document.createElement("header");
+  header.innerHTML = `
+        <a href="/" class="logo">
+            <img src="/assets/images/logo.png" alt="logo">
+        </a>
+
+        <nav>
+            <ul>
+            ${
+              user.is_admin
+                ? `
+                <li><a href="index.html">Home</a></li>
+                <li><a href="view_books.html">View Books</a></li>
+                <li><a href="add_book.html">Add Book</a></li>
+                <li><a href="customers.html">Customers</a></li> `
+                : `       
+                <li><a href="books.html">Books</a></li>
+                <li><a href="borrowed.html">Borrowed Books</a></li>
+            `
+            }
+                <li><a href="profile.html">Profile</a></li>
+            </ul>
+            <button>Log out</button>
+        </nav>
+    
+    `;
+  document.querySelector("body").prepend(header);
+}
